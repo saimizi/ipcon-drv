f5c5e610d622816e21e4a76711e65242da333ef0
Seimizu Joukan <joukan.seimizu@gmail.com>
2025-08-02 19:16:40 +0900

    Update tests.

--- test/sys_test/test_reg_group.c
+++ test/sys_test/test_reg_group.c
@@ -9,91 +9,92 @@ static struct nl_sock *sock = NULL;
 
 static int register_peer(const char *name)
 {
-    struct nl_msg *msg;
-    int ret;
+	struct nl_msg *msg;
+	int ret;
 
-    msg = nlmsg_alloc_simple(IPCON_PEER_REG, 0);
-    if (!msg) {
-        fprintf(stderr, "Failed to allocate netlink message\n");
-        return -1;
-    }
+	msg = nlmsg_alloc_simple(IPCON_PEER_REG, 0);
+	if (!msg) {
+		fprintf(stderr, "Failed to allocate netlink message\n");
+		return -1;
+	}
 
-    ret = nla_put_string(msg, IPCON_ATTR_PEER_NAME, name);
-    if (ret < 0) {
-        fprintf(stderr, "Failed to add peer name attribute\n");
-        nlmsg_free(msg);
-        return -1;
-    }
+	ret = nla_put_string(msg, IPCON_ATTR_PEER_NAME, name);
+	if (ret < 0) {
+		fprintf(stderr, "Failed to add peer name attribute\n");
+		nlmsg_free(msg);
+		return -1;
+	}
 
-    ret = nl_send_auto(sock, msg);
-    nlmsg_free(msg);
-    if (ret < 0) {
-        fprintf(stderr, "Failed to send peer registration message\n");
-        return -1;
-    }
+	ret = nl_send_auto(sock, msg);
+	nlmsg_free(msg);
+	if (ret < 0) {
+		fprintf(stderr, "Failed to send peer registration message\n");
+		return -1;
+	}
 
-    return 0;
+	return 0;
 }
 
 static int register_group(const char *group_name)
 {
-    struct nl_msg *msg;
-    int ret;
+	struct nl_msg *msg;
+	int ret;
 
-    msg = nlmsg_alloc_simple(IPCON_GRP_REG, 0);
-    if (!msg) {
-        fprintf(stderr, "Failed to allocate netlink message\n");
-        return -1;
-    }
+	msg = nlmsg_alloc_simple(IPCON_GRP_REG, 0);
+	if (!msg) {
+		fprintf(stderr, "Failed to allocate netlink message\n");
+		return -1;
+	}
 
-    ret = nla_put_string(msg, IPCON_ATTR_GROUP_NAME, group_name);
-    if (ret < 0) {
-        fprintf(stderr, "Failed to add group name attribute\n");
-        nlmsg_free(msg);
-        return -1;
-    }
+	ret = nla_put_string(msg, IPCON_ATTR_GROUP_NAME, group_name);
+	if (ret < 0) {
+		fprintf(stderr, "Failed to add group name attribute\n");
+		nlmsg_free(msg);
+		return -1;
+	}
 
-    ret = nl_send_auto(sock, msg);
-    nlmsg_free(msg);
-    if (ret < 0) {
-        fprintf(stderr, "Failed to send group registration message\n");
-        return -1;
-    }
+	ret = nl_send_auto(sock, msg);
+	nlmsg_free(msg);
+	if (ret < 0) {
+		fprintf(stderr, "Failed to send group registration message\n");
+		return -1;
+	}
 
-    return 0;
+	return 0;
 }
 
-int main() {
-    int ret = 0;
+int main()
+{
+	int ret = 0;
 
-    sock = nl_socket_alloc();
-    if (!sock) {
-        printf("TEST FAILED: Could not allocate netlink socket\n");
-        return 1;
-    }
+	sock = nl_socket_alloc();
+	if (!sock) {
+		printf("TEST FAILED: Could not allocate netlink socket\n");
+		return 1;
+	}
 
-    if (nl_connect(sock, NETLINK_IPCON) < 0) {
-        printf("TEST FAILED: Could not connect netlink socket\n");
-        nl_socket_free(sock);
-        return 1;
-    }
+	if (nl_connect(sock, NETLINK_IPCON) < 0) {
+		printf("TEST FAILED: Could not connect netlink socket\n");
+		nl_socket_free(sock);
+		return 1;
+	}
 
-    if (register_peer("test_peer") != 0) {
-        printf("TEST FAILED: Could not register peer\n");
-        ret = 1;
-        goto out;
-    }
+	if (register_peer("test_peer") != 0) {
+		printf("TEST FAILED: Could not register peer\n");
+		ret = 1;
+		goto out;
+	}
 
-    if (register_group("test_group") != 0) {
-        printf("TEST FAILED: Could not register group\n");
-        ret = 1;
-        goto out;
-    }
+	if (register_group("test_group") != 0) {
+		printf("TEST FAILED: Could not register group\n");
+		ret = 1;
+		goto out;
+	}
 
-    printf("TEST PASSED: Group registration successful\n");
+	printf("TEST PASSED: Group registration successful\n");
 
 out:
-    nl_close(sock);
-    nl_socket_free(sock);
-    return ret;
+	nl_close(sock);
+	nl_socket_free(sock);
+	return ret;
 }
--- test/sys_test/test_reg_peer.c
+++ test/sys_test/test_reg_peer.c
@@ -9,57 +9,79 @@ static struct nl_sock *sock = NULL;
 
 static int register_peer(const char *name)
 {
-    struct nl_msg *msg;
-    int ret;
+	struct nl_msg *msg;
+	int ret;
+	struct sockaddr_nl dst;
 
-    msg = nlmsg_alloc_simple(IPCON_PEER_REG, 0);
-    if (!msg) {
-        fprintf(stderr, "Failed to allocate netlink message\n");
-        return -1;
-    }
+	memset(&dst, 0, sizeof(dst));
+	dst.nl_family = AF_NETLINK;
+	dst.nl_pid = 0;
+	dst.nl_groups = 0;
 
-    ret = nla_put_string(msg, IPCON_ATTR_PEER_NAME, name);
-    if (ret < 0) {
-        fprintf(stderr, "Failed to add peer name attribute\n");
-        nlmsg_free(msg);
-        return -1;
-    }
+	msg = nlmsg_alloc();
+	if (!msg) {
+		fprintf(stderr, "Failed to allocate netlink message\n");
+		return -1;
+	}
 
-    ret = nl_send_auto(sock, msg);
-    nlmsg_free(msg);
-    if (ret < 0) {
-        fprintf(stderr, "Failed to send peer registration message\n");
-        return -1;
-    }
+	nlmsg_set_dst(msg, &dst);
+	nlmsg_put(msg, NL_AUTO_PORT, NL_AUTO_SEQ, IPCON_PEER_REG,
+		  IPCONMSG_HDRLEN, NLM_F_REQUEST);
 
-    return 0;
+	ret = nla_put_string(msg, IPCON_ATTR_PEER_NAME, name);
+	if (ret < 0) {
+		fprintf(stderr, "Failed to add peer name attribute\n");
+		nlmsg_free(msg);
+		return -1;
+	}
+
+	ret = nla_put_u32(msg, IPCON_ATTR_SPORT, 1000);
+	ret = nla_put_u32(msg, IPCON_ATTR_RPORT, 1001);
+	ret = nla_put_u32(msg, IPCON_ATTR_FLAG, 0);
+
+	nl_complete_msg(sock, msg);
+
+	ret = nl_send_auto(sock, msg);
+	nlmsg_free(msg);
+	if (ret < 0) {
+		fprintf(stderr, "Failed to send peer registration message\n");
+		return -1;
+	}
+	ret = nl_recvmsgs_default(sock);
+	if (ret < 0) {
+		fprintf(stderr, "Failed to send peer registration message\n");
+		return -1;
+	}
+
+	return 0;
 }
 
-int main() {
-    int ret = 0;
+int main()
+{
+	int ret = 0;
 
-    sock = nl_socket_alloc();
-    if (!sock) {
-        printf("TEST FAILED: Could not allocate netlink socket\n");
-        return 1;
-    }
+	sock = nl_socket_alloc();
+	if (!sock) {
+		printf("TEST FAILED: Could not allocate netlink socket\n");
+		return 1;
+	}
 
-    if (nl_connect(sock, NETLINK_IPCON) < 0) {
-        printf("TEST FAILED: Could not connect netlink socket\n");
-        nl_socket_free(sock);
-        return 1;
-    }
+	if (nl_connect(sock, NETLINK_IPCON) < 0) {
+		printf("TEST FAILED: Could not connect netlink socket\n");
+		nl_socket_free(sock);
+		return 1;
+	}
 
-    if (register_peer("test_peer") != 0) {
-        printf("TEST FAILED: Could not register peer\n");
-        ret = 1;
-        goto out;
-    }
+	if (register_peer("test_peer") != 0) {
+		printf("TEST FAILED: Could not register peer\n");
+		ret = 1;
+		goto out;
+	}
 
-    printf("TEST PASSED: Peer registration successful\n");
+	printf("TEST PASSED: Peer registration successful\n");
 
 out:
-    nl_close(sock);
-    nl_socket_free(sock);
-    return ret;
+	nl_close(sock);
+	nl_socket_free(sock);
+	return ret;
 }
